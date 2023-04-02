c81c23a03338d70fcb521f60ead1d929c807e3df
Superburas <barwoj14@wp.pl>
2023-04-02 20:00:56 +0200

    Praca domowa dzień 4.

--- challengeApp/challengeApp/Program.cs
+++ challengeApp/challengeApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var TwojeImię = "Ewa";
+var TwojaPłeć = "kobieta";
+var TwójWiek = 17;
+string Tekst1 = " - jesteś 33 letnią kobietą.";
+string Tekst2 = " - jesteś kobietą poniżej 30 lat.";
+string result1 = TwojeImię + Tekst1;
+string result2 = TwojeImię + Tekst2;
+
+if (TwojaPłeć == "kobieta" && TwójWiek == 33)
+{
+    Console.WriteLine(result1);
+}
+else if (TwojaPłeć == "kobieta" && TwójWiek < 30)
+{
+    Console.Write(result2);
+}
+else if (!(TwojaPłeć == "kobieta") && TwójWiek < 18)
+{
+    Console.WriteLine("Jesteś niepełnoletnim młodzieńcem.");
+}
+else if (TwójWiek > 30)
+{
+    Console.Write("Jesteś juz po trzydziestce");
+}
