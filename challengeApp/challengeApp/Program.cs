9ff95ec82394b8f237d48ac77c73c72a4dc08f1a
SavraX <Barwoj14@wp.pl>
2023-04-05 12:59:34 +0200

    Zadanie domowe dzień 4. Zmiana wielkości liter.

--- challengeApp/challengeApp/Program.cs
+++ challengeApp/challengeApp/Program.cs
@@ -1,24 +1,24 @@
-﻿string YourName = "Ewa";
-string YourSex = "kobieta";
-var YourAge = 33;
-string Text1 = " - jesteś 33 letnią kobietą.";
-string Text2 = " - jesteś kobietą poniżej 30 lat.";
-string result1 = YourName + Text1;
-string result2 = YourName + Text2;
+﻿string name = "Ewa";
+string sex = "kobieta";
+var age = 60;
+string text1 = " - jesteś 33 letnią kobietą.";
+string text2 = " - jesteś kobietą poniżej 30 lat.";
+string result1 = name + text1;
+string result2 = name + text2;
 
-if (YourSex == "kobieta" && YourAge == 33)
+if (sex == "kobieta" && age == 33)
 {
     Console.WriteLine(result1);
 }
-else if (YourSex == "kobieta" && YourAge < 30)
+else if (sex == "kobieta" && age < 30)
 {
     Console.Write(result2);
 }
-else if (!(YourSex == "kobieta") && YourAge < 18)
+else if (!(sex == "kobieta") && age < 18)
 {
     Console.WriteLine("Jesteś niepełnoletnim młodzieńcem.");
 }
-else if (YourAge > 30)
+else if (age > 30)
 {
     Console.Write("Jesteś juz po trzydziestce");
 }
