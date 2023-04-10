4f1bd6d8dbfa8274f123c127bdeeb0c87405dd6b
Superburas <barwoj14@wp.pl>
2023-04-10 19:57:21 +0200

    Zadanie domowe dzień 5, Ile razy cyfra wystepuje w liczbie.

--- challengeApp/challengeApp/Program.cs
+++ challengeApp/challengeApp/Program.cs
@@ -1,24 +1,68 @@
-﻿string name = "Ewa";
-string sex = "kobieta";
-var age = 60;
-string text1 = " - jesteś 33 letnią kobietą.";
-string text2 = " - jesteś kobietą poniżej 30 lat.";
-string result1 = name + text1;
-string result2 = name + text2;
+﻿        var number = 9459554994;
+        string numberAsString = number.ToString();
+        char[] letters = numberAsString.ToCharArray();
+        var counter0 = 0;
+        var counter1 = 0;
+        var counter2 = 0;
+        var counter3 = 0;
+        var counter4 = 0;
+        var counter5 = 0;
+        var counter6 = 0;
+        var counter7 = 0;
+        var counter8 = 0;
+        var counter9 = 0;
 
-if (sex == "kobieta" && age == 33)
+foreach (char letter in letters)
 {
-    Console.WriteLine(result1);
+            if (letter == '0')
+            {
+                counter0++;
+            }
+            else if (letter == '1')
+            {
+                counter1++;
+            }
+            else if (letter == '2')
+            {
+                counter2++;
+            }
+            else if (letter == '3')
+            {
+                counter3++;
+            }
+            else if (letter == '4')
+            {
+                counter4++;
+            }
+            else if (letter == '5')
+            {
+                counter5++;
+            }
+            else if (letter == '6')
+            {
+                counter6++;
+            }
+            else if (letter == '7')
+            {
+                counter7++;
+            }
+            else if (letter == '8')
+            {
+                counter8++;
+            }
+            else
+            {
+                counter9++;
+            }
 }
-else if (sex == "kobieta" && age < 30)
-{
-    Console.Write(result2);
-}
-else if (!(sex == "kobieta") && age < 18)
-{
-    Console.WriteLine("Jesteś niepełnoletnim młodzieńcem.");
-}
-else if (age > 30)
-{
-    Console.Write("Jesteś juz po trzydziestce");
-}
+Console.WriteLine("0 w podanej przez Ciebie liczbie występuje " + counter0 + " razy");
+Console.WriteLine("1 w podanej przez Ciebie liczbie występuje " + counter1 + " razy");
+Console.WriteLine("2 w podanej przez Ciebie liczbie występuje " + counter2 + " razy");
+Console.WriteLine("3 w podanej przez Ciebie liczbie występuje " + counter3 + " razy");
+Console.WriteLine("4 w podanej przez Ciebie liczbie występuje " + counter4 + " razy");
+Console.WriteLine("5 w podanej przez Ciebie liczbie występuje " + counter5 + " razy");
+Console.WriteLine("6 w podanej przez Ciebie liczbie występuje " + counter6 + " razy");
+Console.WriteLine("7 w podanej przez Ciebie liczbie występuje " + counter7 + " razy");
+Console.WriteLine("8 w podanej przez Ciebie liczbie występuje " + counter8 + " razy");
+Console.WriteLine("9 w podanej przez Ciebie liczbie występuje " + counter9 + " razy");
+      
